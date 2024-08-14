7651b49938078139349715870db7db394922d567
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-14 17:56:57 +0300

    Commit for changes

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const dotenv = require("dotenv");
 dotenv.config();
+
+const sequelize = require(`./sequelize`);
+
 const app = require(`./app`);
+
+// Sync Database
+async function testDB() {
+  try {
+    await sequelize.authenticate({});
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+testDB(); // This code will test the connection
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
