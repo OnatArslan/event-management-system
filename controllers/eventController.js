662a7502cbaf398e1980956ba3092e4c1ddd8f29
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-14 16:44:30 +0300

    Commit for prisma orm

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,7 +1,34 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
 exports.getAllEvents = async (req, res, next) => {
   try {
+    await prisma.user.create({
+      data: {
+        name: "Alice",
+        email: "onat@prisma.io",
+        posts: {
+          create: { title: "Hello World" },
+        },
+        profile: {
+          create: { bio: "I like turtles" },
+        },
+      },
+    });
+
+    const allUsers = await prisma.user.findMany({
+      include: {
+        posts: true,
+        profile: true,
+      },
+    });
+
     res.status(200).json({
       status: `success`,
+      data: {
+        allUsers,
+      },
     });
   } catch (error) {
     next(error);
