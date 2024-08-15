ebb7f283930748a79dec90ee6b05a8b570bd8820
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-15 18:27:28 +0300

    Commit for changes on sequelize setup

--- sequelize.js
+++ sequelize.js
@@ -1,5 +1,1 @@
-const { Sequelize } = require(`sequelize`);
 
-const sequelize = new Sequelize(`${process.env.DATABASE_URL}`);
-
-module.exports = sequelize;
--- server.js
+++ server.js
@@ -5,17 +5,6 @@ const sequelize = require(`./sequelize`);
 
 const app = require(`./app`);
 
-// Sync Database
-async function testDB() {
-  try {
-    await sequelize.authenticate({});
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-}
-testDB(); // This code will test the connection
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
