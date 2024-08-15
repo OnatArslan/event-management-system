1fac07e1bb0847682f12bd373e2f87cdef1a6bcf
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-15 19:32:14 +0300

    commit for check password and confirmation fields

---
+++ models/user.js
@@ -0,0 +1,90 @@
+const { Sequelize, DataTypes, Model } = require(`sequelize`);
+const { sequelize } = require(`../database/connection`);
+
+class User extends Model {}
+
+User.init(
+  {
+    // Fields
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: {
+          args: [4, 20],
+          msg: `Username length must be between 4 and 20 characters`,
+        },
+        isAlphanumeric: {
+          args: true,
+          msg: `Username must contain only letters and numbers`,
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: `This field must contain a valid email address`,
+        },
+        notEmpty: {
+          args: true,
+          msg: `Email cannot be empty`,
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [8, 30],
+          msg: "Password must be between 8 and 30 characters",
+        },
+        isComplex(value) {
+          const regex =
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/;
+          if (!regex.test(value)) {
+            throw new Error(
+              "Password must include uppercase, lowercase, number, and special character"
+            );
+          }
+        },
+      },
+    },
+    passwordConfirmation: {
+      type: DataTypes.VIRTUAL,
+      allowNull: false,
+      validate: {
+        isEqual(value) {
+          if (this.password.toString() !== value.toString()) {
+            throw new Error(`Password confirmation does not match password`);
+          }
+        },
+      },
+    },
+  },
+  {
+    // Options
+    sequelize, // sequelize object must be in options
+    modelName: `User`,
+    tableName: `users`,
+    timestamps: true,
+    paranoid: true,
+    indexes: [
+      {
+        unique: true,
+        fields: [`username`],
+      },
+    ],
+  }
+);
