b3f9f38ebd8f7bb08b142fc57aa638bb23c01a23
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-15 18:38:24 +0300

    Commit for database test functions

--- database/connection.js
+++ database/connection.js
@@ -1,1 +1,27 @@
+const { env } = require("process");
 const { Sequelize } = require("sequelize");
+
+// Get sequelize instances
+const sequelize = new Sequelize(`${env(`DATABASE_URL`)}`);
+
+// Add neccessary functions (use them in server.js)
+
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(`Connection has been established succesfully.`);
+  } catch (error) {
+    console.error(`Unable to connect to the database`, error);
+  }
+};
+
+const syncDatabase = async () => {
+  try {
+    await sequelize.sync({ alter: true });
+    console.log(`Database synchronized succesfully.`);
+  } catch (error) {
+    console.error(`Error synchronizing the database.`, error);
+  }
+};
+
+module.exports = { sequelize, testConnection, syncDatabase };
