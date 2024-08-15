37998780d096262b00743403eba538d643b241ab
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-15 18:40:16 +0300

    Commit for changes on index.js

---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+const { Sequelize, DataTypes } = require(`sequelize`);
+const { sequelize } = require(`../database/connection`);
+
+// Import models here
+
+// Add associations here
+
+// module.exports = {Model1, Model2, sequelize}
