9a9d270807bd7dcb212274cc5f1f220658f62dee
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-17 05:13:50 +0300

    Commit for event controllers(started)

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,4 +1,50 @@
+const Event = require("../models/event");
+
 exports.getAllEvents = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: `success`,
+      data: {},
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getEvent = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: `success`,
+      data: {},
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createEvent = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: `success`,
+      data: {},
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateEvent = async (req, res, next) => {
+  try {
+    res.status(200).json({
+      status: `success`,
+      data: {},
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteEvent = async (req, res, next) => {
   try {
     res.status(200).json({
       status: `success`,
--- models/index.js
+++ models/index.js
@@ -6,5 +6,8 @@ const User = require(`./user`);
 const Event = require(`./event`);
 // Add associations here
 
+Event.belongsTo(User, { foreignKey: `organizerId`, as: `organizer` });
+User.hasMany(Event, { foreignKey: `organizerId`, as: `events` });
+
 // Export modules centeral
 module.exports = { User, sequelize, Event };
