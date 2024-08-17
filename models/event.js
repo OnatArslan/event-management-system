c5de774529c01997d8c19bf9608bb5d19677a55e
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-17 04:39:38 +0300

    Commit for Event model

--- models/event.js
+++ models/event.js
@@ -4,11 +4,60 @@ const { sequelize } = require(`../database/connection`);
 class Event extends Model {}
 
 Event.init(
-  {},
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: `Title cannot be empty`,
+        },
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    coordinates: {
+      type: DataTypes.GEOMETRY("POINT"),
+      allowNull: true,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    time: {
+      type: DataTypes.TIME,
+      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
   {
     sequelize,
     modelName: `Event`,
     tableName: `events`,
     timestamps: true,
+    indexes: [
+      {
+        unique: true,
+        fields: [`title`, `date`],
+      },
+    ],
   }
 );
+
+module.exports = Event;
