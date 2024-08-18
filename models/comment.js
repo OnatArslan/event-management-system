e3266e390466ca338c643b06db1b95e6aa41ca71
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-18 07:36:50 +0300

    Commit for comment model

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,29 @@ const { sequelize } = require(`../database/connection`);
 class Comment extends Model {}
 
 Comment.init(
-  {},
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    eventId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    parentCommentId: {
+      type: DataTypes.UUID,
+    },
+  },
   {
     sequelize,
     paranoid: true,
