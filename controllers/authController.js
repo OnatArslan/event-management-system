b3160a1e7dc2bcae71752694acc3e68bcb495098
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-20 05:56:48 +0300

    Commit for login user (not finished)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,14 @@
 const { User } = require("../models/index");
 const jwt = require("jsonwebtoken");
+const bcrypt = require(`bcrypt`);
 
 exports.signUp = async (req, res, next) => {
   try {
     const { username, email, password, passwordConfirmation, profileInfo } =
       req.body;
+    if (!username || !email || !password || !passwordConfirmation) {
+      return next(new Error(`Missing credentials`));
+    }
     const newUser = await User.create(
       {
         username,
@@ -21,11 +25,16 @@ exports.signUp = async (req, res, next) => {
     const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET_KEY, {
       expiresIn: `2 days`,
     });
+    // Send token via cookie
+    res.cookie(`token`, token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      maxAge: 2 * 24 * 60 * 60 * 1000, // 2 days in milliseconds
+    });
+    // Send response with message
     res.status(200).json({
-      data: {
-        user: newUser,
-        token: token,
-      },
+      status: `success`,
+      message: `${newUser.username} is created successfuly`,
     });
   } catch (err) {
     next(err);
@@ -34,8 +43,31 @@ exports.signUp = async (req, res, next) => {
 
 exports.signIn = async (req, res, next) => {
   try {
+    const { email, password } = req.body;
+    // If email or password is not in req.body return Error
+    if (!email || !password) {
+      return next(new Error(`Missing credentials!`));
+    }
+    // Get user with given email
+    const user = await User.scope(`withAll`).findOne({
+      where: { email: email },
+    });
+    // If user with given email is not found return Error
+    if (!user) {
+      return next(new Error(`User not found`));
+    }
+    // Compare password
+    const match = await bcrypt.compare(password, user.password);
+    // If password is not correct return Error
+    if (!match) {
+      return next(new Error(`Invalid credentials`));
+    }
+    console.log(`Hello world`);
+
     res.status(200).json({
       data: `hello`,
     });
-  } catch (error) {}
+  } catch (err) {
+    next(err);
+  }
 };
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const router = express.Router();
 
-router.route(`/signIn`).get(authController.signIn);
+router.route(`/signIn`).post(authController.signIn);
 router.route(`/signUp`).post(authController.signUp);
 
 module.exports = router;
