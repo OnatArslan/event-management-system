09c78d66f488b1ef630d2617b27aee5cfac220b5
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-20 08:48:57 +0300

    Commit for start event routers and controller(they are done before but so simple,after auth controler I will get them to last parts)

--- routers/eventRouter.js
+++ routers/eventRouter.js
@@ -1,23 +1,29 @@
 const express = require("express");
-
+// Import controllers
 const eventController = require("../controllers/eventController");
 
+// Import routers
 const commentRouter = require(`./commentRouter`);
 const reviewRouter = require(`./reviewRouter`);
+
+// Import auth middlewares
+const auth = require(`../middlewares/authMiddleware`);
 
 const router = express.Router();
 
 router.use(`/:eventId/comments`, commentRouter);
 router.use(`/:eventId/reviews`, reviewRouter);
 
-router
-  .route(`/`)
-  .get(eventController.getAllEvents)
-  .post(eventController.createEvent);
+// These router do not have auth validation
+router.route(`/`).get(eventController.getAllEvents);
+router.route(`/:eventId`).get(eventController.getEvent);
+
+// Protect these routes and restrict to 'admin' and 'organizer'
+router.use(auth.protect, auth.restrict([`admin`, `organizer`]));
+router.route(`/`).post(eventController.createEvent);
 
 router
   .route("/:eventId")
-  .get(eventController.getEvent)
   .patch(eventController.updateEvent)
   .delete(eventController.deleteEvent);
 
