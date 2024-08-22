52bd932f085368e60e8c6ad0e3c9876184ffda0e
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-08-22 17:20:09 +0300

    Commit for comment router

--- routers/commentRouter.js
+++ routers/commentRouter.js
@@ -6,7 +6,7 @@ const commentController = require(`../controllers/commentController`);
 // all routes in this route like api/v1/events/:eventId/comments/:commentId format
 const router = express.Router({ mergeParams: true });
 
-// this route is for 127.0.0.1:3000/api/v1/events/:eventId/reviews
+// this route is for 127.0.0.1:3000/api/v1/events/:eventId/omments
 router
   .route(`/`)
   .get(commentController.getAllComments)
